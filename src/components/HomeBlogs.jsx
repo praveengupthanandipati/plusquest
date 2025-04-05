4c9dc565e7c982e3ad8a023965bfcbe6d8348794
Praveen <praveennandipati@gmail.com>
2025-04-05 17:21:19 +0530

    commit files

---
+++ src/components/HomeBlogs.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Blog01Img from "../assets/images/blog/blog01img.jpg";
+import Blog02Img from "../assets/images/blog/blog02img.jpg";
+import Blog03Img from "../assets/images/blog/blog03img.jpg";
+import Blog04Img from "../assets/images/blog/blog04img.jpg";
+
+
+const HomeBlogs = () => {
+    const blogItems =[
+        {
+            image:Blog01Img,
+            date:"4 days ago",
+            title:"The 7 Must-Learn JavaScript Frameworks and Libraries for Modern Web Developers in 2024",
+            link:"/blog"
+        },
+        {
+            image:Blog02Img,
+            date:"4 days ago",
+            title:"The 7 Must-Learn JavaScript Frameworks and Libraries for Modern Web Developers in 2024",
+            link:"/blog"
+        },
+        {
+            image:Blog03Img,
+            date:"4 days ago",
+            title:"The 7 Must-Learn JavaScript Frameworks and Libraries for Modern Web Developers in 2024",
+            link:"/blog"
+        },
+        {
+            image:Blog04Img,
+            date:"4 days ago",
+            title:"The 7 Must-Learn JavaScript Frameworks and Libraries for Modern Web Developers in 2024",
+            link:"/blog"
+        },
+    ]
+  return (
+    <div className="row pt-3 pt-md-5">
+        {blogItems.map((blogItem)=>(
+            <div className="col-md-3 pb-4">
+                <Link to={blogItem.link} className="blogCard">
+                  <figure>
+                     <img src={blogItem.image} alt={blogItem.title} className="img-fluid" />
+                  </figure>
+                  <article>
+                    <p class="p-0 m-0"><small className="text-primary-color">{blogItem.date}</small></p>
+                    <h5 className="text-secondary-color font-semibold">{blogItem.title}</h5>
+                  </article>
+                </Link>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default HomeBlogs
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeBanner from '../components/HomeBanner';
 import HomeServices from '../components/HomeServices';
+import HomeBlogs from "../components/HomeBlogs";
 import About01Image from "../assets/images/aboutimg01.jpg";
 import About02Image from "../assets/images/aboutimg02.jpg";
+import WhyImageHome from "../assets/images/whyimg.png";
 
 
 const Home = () => {
   const aboutWelcome="Welcome";
   const aboutTitle="Pluse Quest";
-  const headingtitle="At Pluse Quest, we bring innovation, expertise, and passion to help businesses thrive in the digital age.";
+  const headingtitle="Transforming businesses through digital innovation";
   const aboutDesc="Whether you need skilled resources for Microsoft Power Platform and Dynamics 365 Customer Engagement, impactful digital marketing strategies, or stunning web and mobile app development, we’re your trusted technology partner."
   const serviceWelcome="Services We Offer";
   const serviceMainTitle ="Certified Excellence";
   const serviceSectiontitledesc="With a team of certified professionals and industry experts, we help clients accelerate digital transformation using the Microsoft ecosystem, modern marketing techniques, and user-centric development.";
+
+  const whyChooseCards=[
+    {
+      icon:"person-check",
+      title:"Certified Experts in Microsoft Power Platform and Dynamics 365",
+      desc:"Our skilled developers use the latest technologies to build fast, secure, and scalable websites tailored to your business needs for maximum growth."
+    },
+    {
+      icon:"window-split",
+      title:"Flexible Engagement Models – from project-based to staff augmentation",
+      desc:"We create unique, responsive websites and apps that align with your brand identity, ensuring an engaging user experience and higher conversions."
+    },
+    {
+      icon:"palette2",
+      title:"Client-Centric Approach – We listen, understand, and deliver",
+      desc:"Trusted by leading brands, we deliver high-performing digital solutions backed by data-driven strategies and measurable success."
+    },
+    {
+      icon:"briefcase",
+      title:"Scalable Solutions tailored to your business needs",
+      desc:"Trusted by leading brands, we deliver high-performing digital solutions backed by data-driven strategies and measurable success."
+    },
+  ]
+
   return (
     <>
         <main>
@@ -65,6 +91,52 @@ const Home = () => {
                 </div>
             </section>
 
+            <section className="whyChooseSection homeSection">
+              <div className="container">
+                  <h2 class="sectiontitle font-bold position-relative text-secondary-color">Why Choose Pluse Quest</h2>
+                  <div className="row pt-2 pt-md-5">
+                      <div className="col-md-6">
+                          <img src={WhyImageHome} className="img-fluid"/>
+                      </div>
+                      <div className="col-md-6 align-self-center">
+                        
+                          {whyChooseCards.map((whycard)=>(
+                           <div className="pb-2 whyrow row">
+                              <div class="col-md-2">
+                                  <span className="icon d-flex justify-content-center align-items-center mx-auto">
+                                      <i class={`bi bi-${whycard.icon}`}></i>
+                                  </span>
+                              </div>
+                              <div class="col-md-10">
+                                  <article >
+                                    <h6 className='font-semibold pb-2 text-secondary-color'>{whycard.title}</h6>
+                                    <p>{whycard.desc}</p>
+                                  </article>
+                              </div>                             
+                           </div>       
+                           ))}                 
+                      </div>
+                  </div>
+              </div>
+            </section>
+
+            <section className="homeSection blogSection">
+                <div className="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2 class="sectiontitle font-bold position-relative text-secondary-color">White Papers, Blog, News</h2>
+                        </div>
+                        <div className="col-md-6 text-end">
+                            <Link className="btn-green text-white" to="">
+                                <span className="text-uppercase"> View All</span>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <HomeBlogs/>
+                    
+                </div>
+            </section>
             
 
         </main>
